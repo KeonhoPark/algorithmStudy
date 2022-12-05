5fa53ffdd1f5db9aaadf9726bed195b1007bfe8c
KeonhoPark <gse04063@gmail.com>
2022-12-05 17:26:29 +0900

    인프런 섹션5 - 공주구하기(큐를 이용한 풀이)

--- INF_05_05.py
+++ INF_05_05.py
@@ -1,21 +1,34 @@
+from collections import deque
+
 n, k = map(int, input().split())
 data = list()
+
 for i in range(1, n+1):
     data.append(i)
 
-i = 0
+queue = deque(data)
+
+# i = 0
+# count = 1
+# while len(data) > 1:
+#     count += 1
+#     i += 1
+#     if count == k:
+#         if i > len(data) - 1:
+#             i = i % len(data)
+#         data.pop(i)
+#         count = 1
+#
+# print(data[0])
+
 count = 1
-while len(data) > 1:
-    count += 1
-    i += 1
+while len(queue) > 1:
     if count == k:
-        if i > len(data) - 1:
-            i = i % len(data)
-        data.pop(i)
-        count = 1
+        queue.popleft()
+        count = 0
+    else:
+        d = queue.popleft()
+        queue.append(d)
+    count += 1
 
-    # if i > len(data)-1:
-    #     count += 1
-    #     i = 0
-
-print(data[0])
+print(queue[0])
