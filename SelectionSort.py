f7b652b8c3fc55e24765798c7ad7f12cbb1a6357
KeonhoPark <gse04063@gmail.com>
2022-12-23 02:34:20 +0900

    Selection Sort 구현 연습

--- SelectionSort.py
+++ SelectionSort.py
@@ -3,11 +3,11 @@ data = list(map(int, input().split()))
 
 def selection_sort_asc(l):
     for i in range(len(l)-1):
-        min = 99999
-        for j in range(i, len(l)):
-            if min > l[j]:
-                min = l[j]
-        min, l[i] = l[i], min
+        min_idx = i
+        for j in range(i+1, len(l)):
+            if l[min_idx] > l[j]:
+                min_idx = j
+        l[min_idx], l[i] = l[i], l[min_idx]
 
 
 if __name__ == "__main__":
