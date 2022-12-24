236073e71c11e1ca9e3cdfecf2201ceeb571aae3
KeonhoPark <gse04063@gmail.com>
2022-12-24 22:43:46 +0900

    인프런 섹션6 - 중복순열 구하기(1)

--- INF_06_06.py
+++ INF_06_06.py
@@ -1,24 +1,23 @@
 n, m = map(int, input().split())
 data = [i for i in range(1, n + 1)]
 res = [0] * m
-
 count = 0
 
 
 def dfs(root):
     global count
     if root == m:
-        count += 1
         for r in res:
             print(r, end=" ")
         print()
+        count += 1
+        return
     else:
         for d in data:
             res[root] = d
-            dfs(root+1)
+            dfs(root + 1)
 
 
-dfs(0)
-print(count)
-
-
+if __name__ == "__main__":
+    dfs(0)
+    print(count)
