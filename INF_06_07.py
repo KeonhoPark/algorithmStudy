38afbd0b0d0217f15f3e82b5e2ec6483438e0501
KeonhoPark <gse04063@gmail.com>
2022-12-24 22:44:03 +0900

    인프런 섹션6 - 동전교환(1)

--- INF_06_07.py
+++ INF_06_07.py
@@ -1,24 +1,23 @@
 n = int(input())
 coin = list(map(int, input().split()))
-coin = sorted(coin, reverse=True)
 m = int(input())
-res = 100000
+coin.sort(reverse=True)
+result = 1e9
 
 
 def dfs(root, sum):
-    global res
+    global result
+    if root > result:
+        return
     if sum > m:
         return
-    if root > res:
-        return
     if sum == m:
-        if root < res:
-            res = root
+        result = root
     else:
         for c in coin:
-            dfs(root + 1, sum + c)
+            dfs(root+1, sum+c)
 
 
-
-dfs(0, 0)
-print(res)
+if __name__=="__main__":
+    dfs(0, 0)
+    print(result)
