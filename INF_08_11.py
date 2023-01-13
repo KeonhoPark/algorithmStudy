e7072d2975a4a258c3d54adc278a2804ef3ad185
KeonhoPark <gse04063@gmail.com>
2023-01-13 16:16:04 +0900

    인프런 섹션8 - 동전교환(냅색 알고리즘)(수정)

--- INF_08_11.py
+++ INF_08_11.py
@@ -1,16 +1,14 @@
-n = int(input())
-coin = list(map(int, input().split()))
-m = int(input())
+if __name__=="__main__":
+    n = int(input())
+    coin = list(map(int, input().split()))
+    m = int(input())
+    if m < min(coin):
+        print(-1)
+    else:
+        dp = [1e9] * (m+1)
 
-dp = [1e9] * (m+1)
-coin.sort()
+        for c in coin:
+            for j in range(c, len(dp)):
+                dp[j] = min(dp[j-c]+1, dp[j])
 
-dp[min(coin)] = 1
-for i in range(min(coin)):
-    dp[i] = 0
-
-for c in coin:
-    for j in range(c, len(dp)):
-        dp[j] = min(dp[j-c]+1, dp[j])
-
-print(dp[-1])
+        print(dp[-1])
