03cea2a7aab952acda88c4ff4940121e56b4c720
KeonhoPark <gse04063@gmail.com>
2023-01-24 03:15:16 +0900

    백준 2565

--- BJ2565.py
+++ BJ2565.py
@@ -1,29 +1,21 @@
 n = int(input())
 data = [0] * 501
+m = 0
 for i in range(n):
     s, e = map(int, input().split())
+    if s > m:
+        m = s
     data[s] = e
+dp = [0] * 501
 
-dp = [1] * 501
-
-
-def dy(n):
-    if n == 1:
-        return 0
-    elif dp[n] != 1:
-        return dp[n]
+for i in range(1, m+1):
+    mx = 0
+    if data[i] == 0:
+        dp[i] = 0
     else:
-        dp[1] = 1
-        for i in range(2, len(dp)):
-            m = -1
-            for j in range(1, i):
-                if data[i] > data[j] and m < dp[j]:
-                    m = dp[j]
-            dp[i] = m + 1
-        return dp[n]
+        for j in range(1, i):
+            if data[i] > data[j] and mx < dp[j]:
+                mx = dp[j]
+        dp[i] = mx + 1
 
-
-dy(n)
-print(dp)
-print(max(dp))
-
+print(n - max(dp))
