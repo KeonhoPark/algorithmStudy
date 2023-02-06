b3fb468ff7f8ede5d0a411ea0f12188bc1d306b7
KeonhoPark <gse04063@gmail.com>
2023-02-06 19:52:04 +0900

    백준 15904

---
+++ BJ15904.py
@@ -0,0 +1,27 @@
+data = list(input())
+res = ''
+
+for d in data:
+    if d == 'U' or d == 'C' or d == 'P':
+        res += d
+
+flag = [0 for _ in range(4)]
+f = 0
+for r in res:
+    if r == 'U':
+        flag[0] = 1
+    if r == 'C' and flag[0] == 1:
+        flag[1] = 1
+    if r == 'P' and flag[1] == 1:
+        flag[2] = 1
+    if r == 'C' and flag[2] == 1:
+        flag[3] = 1
+
+
+if sum(flag) == 4:
+    print("I love UCPC")
+else:
+    print("I hate UCPC")
+
+
+
