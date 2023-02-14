a15f31d4e9b66e537ed599a113fa4d9f1864615c
KeonhoPark <gse04063@gmail.com>
2023-02-14 20:26:01 +0900

    백준 4949

---
+++ BJ4949.py
@@ -0,0 +1,51 @@
+import sys
+
+while True:
+    string = list(sys.stdin.readline())
+    string.pop()
+    if string == ['.']:
+        break
+    else:
+        stack = list()
+        flag = 0
+
+        for s in string:
+            if s == '(' or s == '[':
+                stack.append(s)
+            elif s == ')':
+                if len(stack) != 0:
+                    if stack[-1] == '(':
+                        stack.pop()
+                    else:
+                        break
+                else:
+                    flag = 1
+                    break
+            elif s == ']':
+                if len(stack) != 0:
+                    if stack[-1] == '[':
+                        stack.pop()
+                    else:
+                        break
+                else:
+                    flag = 1
+                    break
+
+        if len(stack) == 0 and flag == 0:
+            print("yes")
+        else:
+            print("no")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
