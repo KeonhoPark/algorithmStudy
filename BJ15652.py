622b210ee594296e6d059558289bc88712846f1d
KeonhoPark <gse04063@gmail.com>
2023-02-16 18:15:10 +0900

    백준 15652

---
+++ BJ15652.py
@@ -0,0 +1,18 @@
+n, m = map(int, input().split())
+res = [0 for _ in range(m)]
+
+
+def dfs(l, s):
+    if l == m:
+        print(*res)
+        return
+    else:
+        for i in range(s, n + 1):
+            res[l] = i
+            dfs(l + 1, i)
+
+
+
+dfs(0, 1)
+
+
