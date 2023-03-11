50ed51542455dc1d8110a075831e92a1e71c8815
KeonhoPark <gse04063@gmail.com>
2023-03-11 22:21:32 +0900

    백준 2206

--- BJ2206.py
+++ BJ2206.py
@@ -9,6 +9,7 @@ q = deque()
 q.append([0, 0, 0])
 dx = [0, 1, 0, -1]
 dy = [1, 0, -1, 0]
+print(res)
 
 while q:
     cor = q.popleft()
@@ -26,9 +27,15 @@ while q:
             if data[new_y][new_x] == 0 and res[new_y][new_x][is_crashed] == 0:
                 res[new_y][new_x][is_crashed] = res[y][x][is_crashed] + 1
                 q.append([new_y, new_x, is_crashed])
+                print(res)
+                print()
             elif data[new_y][new_x] == 1 and is_crashed == 0:
                 res[new_y][new_x][is_crashed + 1] = res[y][x][is_crashed] + 1
                 q.append([new_y, new_x, is_crashed + 1])
+                print(res)
+                print()
+
+print(res)
 
 ans = max(res[c - 1][r - 1][0], res[c - 1][r - 1][1])
 if ans == 0:
