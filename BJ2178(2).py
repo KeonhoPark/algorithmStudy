04f1e3c2de7a4df22fd4938c1fb821f6f32750a8
KeonhoPark <gse04063@gmail.com>
2023-03-29 18:42:54 +0900

    백준 2178(수정)

---
+++ BJ2178(2).py
@@ -0,0 +1,31 @@
+import sys
+from collections import deque
+
+c, r = map(int, sys.stdin.readline().split())
+data = [list(map(int, sys.stdin.readline().strip())) for _ in range(c)]
+sum = [[0 for _ in range(r)] for _ in range(c)]
+sum[0][0] = 1
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+q = deque()
+q.append([0, 0])
+data[0][0] = 0
+
+
+while q:
+    cor = q.popleft()
+    y = cor[0]
+    x = cor[1]
+
+    if y == c - 1 and x == r - 1:
+        break
+
+    for i in range(4):
+        new_y = y + dy[i]
+        new_x = x + dx[i]
+        if 0 <= new_y < c and 0 <= new_x < r and data[new_y][new_x] == 1:
+            q.append([new_y, new_x])
+            data[new_y][new_x] = 0
+            sum[new_y][new_x] = sum[y][x] + 1
+
+print(sum[y][x])
